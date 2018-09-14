45d17f1465e461ed4dfb6b7045e0d5fc015d5ada
zcai2672 <ivan.z.cai@gmail.com>
2018-09-14 15:06:54 +1000

    add mvc

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -16,6 +16,7 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,7 +24,12 @@ namespace WishList
         {
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                if(env.IsDevelopment()){
+                    app.UseDeveloperExceptionPage();
+                }else{
+                    app.UseExceptionHandler("/home/error");
+                }
+                app.UseMvcWithDefaultRoute();
             });
         }
     }
